2e0ffadd19cd51e5c553102b5fea1c43533a12dc
Evan Porter <evan.porter@dotman.ca>
2013-09-04 17:38:34 -0300

    Fixing whitespace

--- src/myexample/core.cljs
+++ src/myexample/core.cljs
@@ -1,19 +1,19 @@
 (ns nodejstest.core
-    (:require [cljs.nodejs :as node]))
+  (:require [cljs.nodejs :as node]))
 
 (def http
-    (node/require "http"))
+  (node/require "http"))
 
 (defn handler [_ res]
-    (.writeHead res 200 {"Content-Type" "text/plain"})
-    (.end res "Hello World!\n"))
- 
+  (.writeHead res 200 {"Content-Type" "text/plain"})
+  (.end res "Hello World!\n"))
+
 (defn -main [& _]
-    (let [server (.createServer http handler)
-          server-url "127.0.0.1"
-          server-port 1337]
-          (.listen server server-port server-url)
-          (println (str "Server running at http://" server-url ":" server-port "/"))))
- 
+  (let [server (.createServer http handler)
+        server-url "127.0.0.1"
+        server-port 1337]
+    (.listen server server-port server-url)
+    (println (str "Server running at http://" server-url ":" server-port "/"))))
+
 (set! *main-cli-fn* -main)
 
