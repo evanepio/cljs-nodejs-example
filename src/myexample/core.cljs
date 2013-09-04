aed735bdf30801620a2b5d974c0894344f213e91
Evan Porter <evan.porter@dotman.ca>
2013-09-03 22:46:15 -0300

    Making the startup function more standard-looking

--- src/myexample/core.cljs
+++ src/myexample/core.cljs
@@ -8,10 +8,10 @@
     (.writeHead res 200 {"Content-Type" "text/plain"})
     (.end res "Hello World!\n"))
  
-(defn start [& _]
+(defn -main [& _]
     (let [server (.createServer http handler)]
           (.listen server 1337 "127.0.0.1")
           (println "Server running at http://127.0.0.1:1337/")))
  
-(set! *main-cli-fn* start)
+(set! *main-cli-fn* -main)
 
