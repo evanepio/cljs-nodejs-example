f980b515094a33286716c89157cc4af7a5690ecb
Evan Porter <evan.porter@dotman.ca>
2013-09-05 21:37:23 -0300

    Moving function called by get to top level instead of being annonymous

--- src/myexample/core.cljs
+++ src/myexample/core.cljs
@@ -4,12 +4,14 @@
 (def express (node/require "express"))
 (def app (. express (createServer)))
 
+(defn hello-world [req res]
+  (.send res "Hello World"))
+
 (defn -main [& args]
   (let [port 3000]
     (doto app
       (.use (. express (logger)))
-      (.get "/" (fn [req res]
-                  (.send res "Hello World")))
+      (.get "/" hello-world)
       (.listen 3000))
     (println (str "Express server started on port: " port))))
 
