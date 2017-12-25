413137cf6ebcce5dc1636b4d66859523537eef0e
Evan Porter <evan.porter@dotman.ca>
2017-12-25 18:48:11 -0400

    Updating Clojure dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1586"]
-                 [org.bodil/cljs-noderepl "0.1.7"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.9.946"]]
   :hooks [leiningen.cljsbuild]
-  :plugins [[lein-cljsbuild "0.3.0"]]
+  :plugins [[lein-cljsbuild "1.1.7"]]
   :cljsbuild {
               :builds [{
                         :source-paths ["src/myexample"]
